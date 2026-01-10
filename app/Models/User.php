6cae0b6b6587d12f4f5f2b214a4d57afe1f9a8c8
neofrankuy21 <neofrankuy21@gmail.com>
2026-01-10 22:12:26 +0800

    Add Roles and change data_names

--- app/Models/User.php
+++ app/Models/User.php
@@ -18,7 +18,11 @@ class User extends Authenticatable
      * @var list<string>
      */
     protected $fillable = [
-        'name',
+        'first_name',
+        'middle_name',
+        'last_name',
+        'status',
+        'role',
         'email',
         'password',
     ];
--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,10 +13,16 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('first_name');
+            $table->string('middle_name');
+            $table->string('last_name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->timestamp('birthdate')->nullable();
+            $table->integer('role')->default(0);
+            $table->integer('status')->default(0);
+            $table->timestamp('deactivation_date')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
